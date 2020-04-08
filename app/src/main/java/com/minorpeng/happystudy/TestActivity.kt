a82809164681fe33600ddc89951925e187049196
MinorPeng <minorpeng@foxmail.com>
2020-04-08 11:25:49 +0800

    完成编程界面

---
+++ app/src/main/java/com/minorpeng/happystudy/TestActivity.kt
@@ -0,0 +1,17 @@
+package com.minorpeng.happystudy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @author MinorPeng
+ * @date 2020/4/8 9:07
+ */
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
